c471c60e9afdf48ad62e94e9d694ff838dbdf546
Dominic Bartl <dominic.bartl.at@gmail.com>
2016-03-31 13:34:27 +0200

    Write and export function to calculate quarter

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * A module that gets the start and end date of a quarter
+ * @module dominicbartl/quarterrange
+ */
+
+/**
+ * @param {Number} q - The index of the quarter (0, 1, 2, 3)
+ * @param {Number} [year] - The year
+ * @returns {{from: Date, to: Date}}
+ */
+module.exports = function (q, year) {
+    // Make sure it's an integer
+    q = parseInt(q);
+    q = q % 4;
+
+    var dateStart = new Date();
+    var dateEnd = new Date();
+
+    if (year) {
+        dateStart.setFullYear(year);
+        dateEnd.setFullYear(year);
+    }
+    dateStart.setDate(1);
+    dateStart.setMonth((q * 3));
+
+    dateEnd.setDate(15);
+    dateEnd.setMonth(((q + 1) * 3));
+    dateEnd.setDate(0);
+
+    return {
+        from: dateStart
+        to: dateEnd
+    };
+}
