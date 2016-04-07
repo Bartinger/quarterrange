b44d91b829a440112684b87db3d37345b3ad6e30
Dominic Bartl <dominic.bartl.at@gmail.com>
2016-04-07 12:29:49 +0200

    Add support for browsers

--- index.js
+++ index.js
@@ -8,29 +8,52 @@
  * @param {Number} [year] - The year
  * @returns {{from: Date, to: Date}}
  */
-module.exports = function (q, year) {
-    // Make sure it's an integer
-    q = parseInt(q);
-    q = q % 4;
 
-    var dateStart = new Date();
-    var dateEnd = new Date();
+"use strict";
 
-    if (year) {
-        dateStart.setFullYear(year);
-        dateEnd.setFullYear(year);
+(function() {
+    var root = this;
+    var previous_quarterrange = root.quarterrange;
+
+    var quarterrange = function (q, year) {
+        // Make sure it's an integer
+        q = parseInt(q);
+        q = q % 4;
+
+        var dateStart = new Date();
+        var dateEnd = new Date();
+
+        if (year) {
+            dateStart.setFullYear(year);
+            dateEnd.setFullYear(year);
+        }
+        dateStart.setDate(1);
+        dateStart.setMonth((q * 3));
+        dateStart.setUTCHours(0, 0, 0, 0);
+
+        dateEnd.setDate(15);
+        dateEnd.setMonth(((q + 1) * 3));
+        dateEnd.setDate(0);
+        dateEnd.setUTCHours(23, 59, 59, 0);
+
+        return {
+            from: dateStart,
+            to: dateEnd
+        };
     }
-    dateStart.setDate(1);
-    dateStart.setMonth((q * 3));
-    dateStart.setUTCHours(0, 0, 0, 0);
 
-    dateEnd.setDate(15);
-    dateEnd.setMonth(((q + 1) * 3));
-    dateEnd.setDate(0);
-    dateEnd.setUTCHours(23, 59, 59, 0);
+    quarterrange.noConflict = function() {
+        root.quarterrange = previous_quarterrange;
+        return quarterrange;
+    }
 
-    return {
-        from: dateStart,
-        to: dateEnd
-    };
-}
+    if( typeof exports !== 'undefined' ) {
+        if( typeof module !== 'undefined' && module.exports ) {
+            exports = module.exports = quarterrange
+        }
+        exports.quarterrange = quarterrange
+    } else {
+        root.quarterrange = quarterrange
+    }
+
+}).call(this);
